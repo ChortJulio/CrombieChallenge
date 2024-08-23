97d82cfc105665f771b66884d39691f9948e84c4
ChortJulio <chortjulio@gmail.com>
2024-08-23 08:19:16 -0300

    Add new custom components
    Se añadieron nuevos componentes personalizados, como lo son el de DatePicker, FormField que agrega el label encima y el controller de react-hook-form y el mask de número de teléfono.

---
+++ src/components/MyPhoneNumberMask.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { IMaskInput } from "react-imask";
+
+interface CustomProps {
+  onChange: (event: { target: { name: string; value: string } }) => void;
+  name: string;
+}
+
+const MyPhoneNumberMask = React.forwardRef<HTMLInputElement, CustomProps>(
+  function MyPhoneNumberMask(props, ref) {
+    const { onChange, ...other } = props;
+    return (
+      <IMaskInput
+        {...other}
+        mask="(#00) 000-0000"
+        definitions={{
+          "#": /[1-9]/,
+        }}
+        inputRef={ref}
+        onAccept={(value: any) =>
+          onChange({ target: { name: props.name, value } })
+        }
+        overwrite
+      />
+    );
+  }
+);
+
+export default MyPhoneNumberMask;
